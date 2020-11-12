eb87b7f24882042f79df329920119c1a75b9fd39
Muhammad Ikhwan Khalid N <nast.industries@gmail.com>
2020-11-12 23:21:32 +0700

    Initial Commit

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.image(image, caption = "Version 1.0", use_column_width=True)
 
 
 #Masukkan Dataset
-df = pd.read_csv('diabetes.CSV ')
+df = pd.read_csv('diabetes.csv ')
 
 #Set Subheader
 st.subheader('Data Information:')
